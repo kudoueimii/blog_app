871a1d6ce4afe2e8c58ee341a2c7d518965b2f6a
bargholz eimii <818_77_@ezweb.ne.jp>
2022-09-27 09:35:56 +0900

    commit for submitting task of blog app

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,4 @@
 class Blog < ApplicationRecord
   validates :title, presence: true
-  validates :content, {presence: true, length: {maximum: 140}}
+  validates :content, {presence: true, length: {in: 1..140}}
 end
